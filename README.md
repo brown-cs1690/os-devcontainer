6081ebc378e1ffe54c6b21d2f623a89fefb077ff
Ayma-n <aymanbt14@gmail.com>
2025-01-23 16:59:57 -0500

    Release Spring 2025 Dev Container

---
+++ README.md
@@ -0,0 +1,9 @@
+# OS Development Container
+This repository contains a Docker/Podman setup for running Weenix, and other Operating Systems assignmnents, on your machine, for both ARM and x86 architectures.
+
+To set it up, please follow instructions in the [Developing Locally Guide](https://github.com/brown-cs1690/handout/wiki/Developing-Locally-Guide) in the Weenix wiki. 
+
+## Acknowledgements
+
+This setup is forked from Naafiyan Ahmed's CS169 Docker setup, which is forked from Vic (Shihang) Li's CS169 Docker setup, which is forked from the development environment setup for CS300 students,
+which was forked from Harvard's CS61.
---
+++ container/podman-build-container.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+cd `dirname $0`
+
+arch="`arch`"
+os="`uname`"
+disk_size=12
+
+# Check if the Podman machine already exists
+if podman machine list | grep -q "^podman-machine-default"; then
+    echo "Podman machine already exists. Jumping straight to building the image..."
+else
+    # Initializing Podman Machine
+    echo "Initializing Podman machine on $os ($arch)..."
+    podman machine init --disk-size="$disk_size"
+    podman machine start
+fi
+
+tag=
+platform=
+while test "$#" -ne 0; do
+    if test "$1" = "-a" -o "$1" = "--arm" -o "$1" = "--arm64"; then
+        if test "`arch`" = "arm64" -o "`arch`" = "aarch64"; then
+            platform=linux/arm64
+            shift
+        else
+            echo "\`podman-build-container.sh --arm\` only works on ARM64 hosts" 1>&2
+            exit 1
+        fi
+    elif test "$1" = "-x" -o "$1" = "--x86-64" -o "$1" = "--x86_64" -o "$1" = "--amd64"; then
+        platform=linux/amd64
+    else
+        armtext=
+        if test "`arch`" = "arm64" -o "`arch`" = "aarch64"; then
+            armtext=" [-a|--arm] [-x|--x86-64]"
+        fi
+        echo "Usage: podman-build-container.sh$armtext" 1>&2
+        exit 1
+    fi
+done
+if test -z "$platform" -a \( "$arch" = "arm64" -o "$arch" = "aarch64" \); then
+    platform=linux/arm64
+elif test -z "$platform"; then
+    platform=linux/amd64
+fi
+if test -z "$tag" -a "$platform" = linux/arm64; then
+    tag=cs1670:arm64
+elif test -z "$tag"; then
+    tag=cs1670:latest
+fi
+
+if test $platform = linux/arm64; then
+    exec podman build -t "$tag" -f Dockerfile.arm64 --platform linux/arm64 .
+else
+    exec podman build -t "$tag" -f Dockerfile --platform linux/amd64 .
+fi
